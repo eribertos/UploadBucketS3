41178010877e70ce4bd24e4ca881eab3295b9a68
Eriberto Silva <eribertos@gmail.com>
2019-05-06 15:43:58 -0300

    Create UploadBucketS3.ps1

---
+++ UploadBucketS3.ps1
@@ -0,0 +1,15 @@
+### Eriberto
+### @eribertos
+### Script para fazer upload de arquivos
+### 2019050601
+
+
+#Carregando Extensao AWS Powershell 
+import-module "C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell\AWSPowerShell.psd1"
+
+#Setando credenciais com acesso ao Bucket
+Set-AWSCredentials -AccessKey INCLUIRACCESSKEY  -SecretKey INCLUIRSECRETKEY -StoreAs default
+
+#Apontando diretorio de origen e destino
+#Exemplo write-s3object -BucketName NOMEDOBUCKET -Folder DIRETORIO-LOCAL -keyprefix DIRETORIO-REMOTO -recurse
+write-s3object -BucketName NOMEBUCKET -Folder DIRETORIO-LOCAL -keyprefix DIRETORIO-REMOTO\ -recurse
